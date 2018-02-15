0c62739bc48c6d2908f490576a2d1332c1de9f04
kevothedevo <kdudley9240@gmail.com>
2018-02-15 16:21:55 -0600

    adds Part 2 services checkpoint

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,29 +3,51 @@
         var SongPlayer = {};
 
         var currentSong = null;
-        ver currentBuzzObject = null;
+/**
+* @desc Buzz object audio file
+* @type {Object}
+*/
+        var currentBuzzObject = null;
 
-        SongPlayer.play = function(song) {
-          if (currentSong !== song) {
-              if (currentBuzzObject) {
-                  currentBuzzObject.stop();
-              }
-              } else if (currentSong === song) {
-                  if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
-                    song.playing = true;
+/**
+* @function setSong
+* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+* @param {Object} song
+*/
+var setSong = function(song) {
+    if (currentBuzzObject) {
+        currentBuzzObject.stop();
+        currentSong.playing = null;
+    }
+
+    currentBuzzObject = new buzz.sound(song.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+    });
+
+    currentSong = song;
+};
+
+SongPlayer.play = function(song) {
+    if (currentSong !== song) {
+    setSong(song);
+    currentBuzzObject.play();
+    song.playing = true;
+} else if (currentSong === song) {
+    if (currentBuzzObject.isPaused()) {
+    currentBuzzObject.play();
                   }
               }
           }
             var currentBuzzObject = new buzz.sound(song.audioUrl, {
-            currentBuzzObject = new buzz.sound(song.audioUrl, {
-                formats: ['mp3'],
-                preload: true
-            });
 
-            currentBuzzObject.play();
           }
         };
+
+        SongPlayer.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+        }
 
         return SongPlayer;
     }
