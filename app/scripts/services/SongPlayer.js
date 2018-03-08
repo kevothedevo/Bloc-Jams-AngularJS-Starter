27caec0185b51715930e57467a5a3984436c5613
kevothedevo <kdudley9240@gmail.com>
2018-03-07 18:23:33 -0600

    Adds the Services Part 3 checkpoint funtionality, though issues persist

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,8 +1,9 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         var SongPlayer = {};
 
-        var currentSong = null;
+        var currentAlbum = Fixtures.getAlbum();
+
 /**
 * @desc Buzz object audio file
 * @type {Object}
@@ -17,7 +18,7 @@
 var setSong = function(song) {
     if (currentBuzzObject) {
         currentBuzzObject.stop();
-        currentSong.playing = null;
+        SongPlayer.currentSong.playing = null;
     }
 
     currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -25,37 +26,73 @@ var setSong = function(song) {
         preload: true
     });
 
-    currentSong = song;
+    SongPlayer.currentSong = song;
 };
 
-  SongPlayer.play = function(song) {
-      if (currentSong !== song) {
-          setSong(song);
-          playSong(song);
-      } else if (currentSong === song) {
-          if (currentBuzzObject.isPaused()) {
-          playSong(song);
-          }
-      }
-};
+
 
 
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
         }
 
+/**
+@function SongPlayer.previous
+@desc Sets song to previous song on list
+*/
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+/**
+@desc Keeps on the current song if current song is first on list else moves forward
+*/
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+
         playSong = function(song) {
           currentBuzzObject.play();
           song.playing = true;
-
-
         }
+/**
+@desc Gets index of the current songs
+@type {Function}
+*/
+var getSongIndex = function(song) {
+    return currentAlbum.song.indexOf(song);
+};
+
+/**
+* @desc Active song object from list of songs
+* @type {Object}
+*/
+SongPlayer.currentSong = null;
+
+        SongPlayer.play = function(song) {
+              song = song || SongPlayer.currentSong;
+              if (SongPlayer.currentSong !== song) {
+                  setSong(song);
+                  playSong(song);
+              } else if (SongPlayer.currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    playSong(song);
+                  }
+              }
+            };
+
 
         return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['Fixture', SongPlayer]);
 })();
